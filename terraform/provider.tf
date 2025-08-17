97b1342bc2deee589d689e2537a1a9f95050f25f
patanfouzi <patanfouzi26@gmail.com>
2025-08-17 11:32:50 +0530

    Create provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+  credentials = file("${path.module}/key.json")
