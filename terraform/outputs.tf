b73ea500ea4b1bcffd4d3c938f3725c0e7aed3ac
patanfouzi <patanfouzi26@gmail.com>
2025-08-19 19:50:50 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,8 +2,8 @@ output "instance_template" {
   value = google_compute_instance_template.vm1_template.name
 }
 
-output "mig_name" {
-  value = google_compute_instance_group_manager.mig.name
+output "regional_mig_name" {
+  value = google_compute_region_instance_group_manager.mig.name
 }
 
 output "load_balancer_ip" {
